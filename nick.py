fd20f8bb9009b0d3152f0cd379b8b327792ebf6f
yuzu-krs <yztimai@icloud.com>
2024-03-20 00:53:19 +0900

    画像をキャプチャで範囲指定しました

--- nick.py
+++ nick.py
@@ -25,8 +25,11 @@ while True:
     screen = pyautogui.screenshot()
     # Convert the output to a numpy array
     screen_array = np.array(screen)
+    
     # Crop out the region we want - height, width, channels   
-    cropped_region = screen_array[25:625, 1122:, :]
+    cropped_region = screen_array[315:1880, 2350:3390, :]
+
+
     # Convert the color channel order
     corrected_colors = cv2.cvtColor(cropped_region, cv2.COLOR_RGB2BGR)
     
